31af0b9692160760593429c6165d5c6c15b8aa82
BuzzC137 <15257854+BuzzC137@users.noreply.github.com>
2025-08-05 01:17:59 -0500

    moving

--- docs/script.js
+++
@@ -1,8 +0,0 @@
-fetch('/api/status')
-  .then(res => res.json())
-  .then(data => {
-    document.getElementById('status').innerText = data.message;
-  })
-  .catch(() => {
-    document.getElementById('status').innerText = 'Error connecting to bot.';
-  });
